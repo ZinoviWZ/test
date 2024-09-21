8693cca5ed6e0b8ac727bb903e5050ee71657d80
ZinoviWZ <3537590008@qq.com>
2024-09-21 21:20:52 +0800

    second commit

--- test_pytest01.py
+++ test_pytest01.py
@@ -1,6 +1,7 @@
 import json
 import pytest
 import yaml
+import allure
 
 @pytest.fixture(scope="class")
 def func():
@@ -8,25 +9,21 @@ def func():
     yield
     print("fixture后置")
 
+@allure.step("这是测试第X步")
 def add(a, b):
     return a + b
+
+@allure.step("这是测试第X步")
+def jian(a, b):
+    return a - b
 
 def getdata():
     with open("./a.yaml", encoding="utf-8") as f:
         param = yaml.safe_load(f)
     return param
 
-@pytest.mark.parametrize("a,b", getdata())
-def test_01(a,b):
-    s = add(a,b)
-    assert 2 == s
-
-@pytest.mark.parametrize("a,b", getdata())
-def test_02(a,b):
-    s = add(a,b)
-    assert 29 == s
-
-
+@allure.epic("这是XXX系统")
+@allure.feature("这是XXX功能模块")
 class Test1:
     def setup_method(self):
         print("类中setup_class")
@@ -34,15 +31,19 @@ class Test1:
     def teardown_method(self):
         print("类中teardown_class")
 
-    @pytest.mark.parametrize("a,b", getdata())
-    def test_03(self, func, a,b):
-        s = add(a,b)
-        assert 2 == s
-
-    @pytest.mark.parametrize("a,b", getdata())
-    def test_04(self, a, b):
+    @allure.story("测试加法场景")
+    @pytest.mark.parametrize("a,b,c", getdata())
+    @allure.title("测试标题：{a} + {b} = {c}")
+    def test_01(self, a, b, c):
         s = add(a, b)
-        assert 7 == s
+        assert c == s
+
+    @allure.story("测试减法场景")
+    @pytest.mark.parametrize("a,b,c", getdata())
+    @allure.title("测试标题：{a} - {b} = {c}")
+    def test_02(self, a, b, c):
+        s = jian(a, b)
+        assert c == s
 
 if __name__ == '__main__':
 
